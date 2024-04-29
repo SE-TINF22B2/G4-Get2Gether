268110ebe514744375ab7de0ade0cd3e57da14ba
ebauersandoval <elke-b-s@gmx.de>
2024-04-29 19:50:20 +0200

    FE-#67: added profile.svg and some refactoring

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,6 +19,7 @@ import {SideMenuComponent} from "./dashboard/side-menu/side-menu.component";
 import {DashboardContentComponent} from "./dashboard/dashboard-content/dashboard-content.component";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
+import { ProfileMenuComponent } from './dashboard/side-menu/profile-menu/profile-menu.component';
 
 function initializeAppFactory(userService: UserService) {
   return () => {
@@ -33,7 +34,8 @@ function initializeAppFactory(userService: UserService) {
     DashboardComponent,
     SplashscreenComponent,
     SideMenuComponent,
-    DashboardContentComponent
+    DashboardContentComponent,
+    ProfileMenuComponent
   ],
   imports: [
     BrowserModule,
