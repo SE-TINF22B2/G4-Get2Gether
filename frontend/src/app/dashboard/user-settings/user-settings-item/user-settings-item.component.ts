7fb474a7f46bb8b24a2d29f068a4732cc492211d
lica200 <johanna@eitel.de>
2024-05-06 17:56:55 +0200

    FE-#99: farbvorschau first try

--- frontend/src/app/dashboard/user-settings/user-settings-item/user-settings-item.component.ts
+++ frontend/src/app/dashboard/user-settings/user-settings-item/user-settings-item.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import {EventOverview} from "../../../../model/event";
 import {User} from "../../../../model/user";
 import {HttpClient} from "@angular/common/http";
 import {UserService} from "../../../../services/user.service";
+import {iconButton} from "material-components-web/index";
 
 @Component({
   selector: 'app-user-settings-item',
@@ -13,18 +14,32 @@ export class UserSettingsItemComponent {
   constructor(
     public userService: UserService,
     private http: HttpClient
-  ) {
-    if (document.getElementById('colorcard')!=null){
-      // @ts-ignore
-      document.getElementById('colorcard')
-        .setAttribute("background",mat.get-theme-color(this.color, primary))
-    }
-  }
+  ) {}
 
   @Input()
   color!: string;
 
-
+  ngOnInit(){
+    console.log(this.color)
+    //set colorcard
+    if (document.getElementById(this.color)!=null) {
+      // @ts-ignore
+      let element: HTMLElement = document.getElementById(this.color)
+      if (this.color == "light") {
+        element.style.backgroundColor= mat.get'var(--light-default-primary)'
+        console.log("in light"+ element.getAttributeNames())
+      } else if (this.color == "dark") {
+        element.style.backgroundColor= 'var(--dark-default-primary)'
+        console.log("in dark")
+      } else {
+        element.style.backgroundColor= 'var(--error-color)'
+        console.log("in error")
+      }
+    }else{
+      console.log("no such element")
+    }
+  }
+  //switch color on change
   switchColor() {
     document.documentElement.setAttribute("theme", this.color);
     this.http.put("http://localhost:8080/user/self", {
