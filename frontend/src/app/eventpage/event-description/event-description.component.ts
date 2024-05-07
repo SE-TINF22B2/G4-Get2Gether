f58b670f80cf797a7343e61e41976be118fc6e24
Sophia Deckhut <deckhut.sophia.a22@student.dhbw-karlsruhe.de>
2024-05-07 21:00:15 +0200

    FE-#73: event-description more responsive

--- frontend/src/app/eventpage/event-description/event-description.component.ts
+++ frontend/src/app/eventpage/event-description/event-description.component.ts
@@ -1,11 +1,23 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Event} from "../../../model/event";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-event-description',
   templateUrl: './event-description.component.html',
   styleUrl: './event-description.component.scss'
 })
-export class EventDescriptionComponent {
+export class EventDescriptionComponent implements OnInit{
   @Input() eventData: Event | undefined ;
+
+  isPhonePortrait = false;
+
+  constructor(public breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
+      .subscribe(result => {
+        this.isPhonePortrait = result.matches;
+      })
+  }
 }
