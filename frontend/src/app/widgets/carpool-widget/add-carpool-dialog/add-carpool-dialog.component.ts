5ef39c3e5053e22ee785d26641fcf300205d2a9e
Sophia Deckhut <deckhut.sophia.a22@student.dhbw-karlsruhe.de>
2024-05-20 14:40:59 +0200

    FE-#133: generate component add-carpool-dialog

---
+++ frontend/src/app/widgets/carpool-widget/add-carpool-dialog/add-carpool-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-carpool-dialog',
+  templateUrl: './add-carpool-dialog.component.html',
+  styleUrl: './add-carpool-dialog.component.scss'
+})
+export class AddCarpoolDialogComponent {
+
+}
